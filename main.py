6c10295e7b53e4662453902b9eece6fe85674281
Andrey Frolov <50494877+Frolov-Andrey2405@users.noreply.github.com>
2023-01-11 14:34:48 +0200

    Code Refactoring
    1. Added the type hint of argument in the function get_ip_by_hostname() to make the code more readable and understandable
    2. Moved input statement into main function.
    3. Also added some white spaces between expressions to make the code more readable.
    4. Also, I change the variable name hostname to meaningful names.
    
    Follow python naming conventions i.e use lowercase letter in function name and use snake case.

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import socket
 
-
-def get_ip_by_hostname():
-    hostname = input('Please enter a website address(URL): ')
-    
+def get_ip_by_hostname(hostname:str)->str:
     try:
-        return f'Hostname: {hostname}\nIP address: {socket.gethostbyname(hostname)}'
+        ip = socket.gethostbyname(hostname)
+        return f'Hostname: {hostname}\nIP address: {ip}'
     except socket.gaierror as error:
         return f'Invalid Hostname - {error}'
 
 
 def main():
-    print(get_ip_by_hostname())
-    
-    
+    hostname = input('Please enter a website address(URL): ')
+    print(get_ip_by_hostname(hostname))
+
 if __name__ == '__main__':
     main()
